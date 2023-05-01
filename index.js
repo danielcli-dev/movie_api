3f7ca43b12b1e01ac9c9ab61e34fe692dd5c727e
danielcli-dev <98434650+danielcli-dev@users.noreply.github.com>
2023-05-01 14:21:06 -0700

    Updated change profile endpoint

--- index.js
+++ index.js
@@ -205,11 +205,17 @@ app.put(
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+
     Users.findOneAndUpdate(
       { Username: req.params.Username },
       {
         $set: {
           Username: req.body.Username,
+          Password: hashedPassword,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
         },
       },
       { new: true }
